dc1ffc062cd5b5c090626180bf04009ee3fc37a7
Stef van 't Schip <stef@netwerven.nl>
2015-07-03 14:31:33 +0200

    basic structure

--- app/Http/Views/index.php
+++ app/Http/Views/index.php
@@ -1,9 +1,34 @@
 <!doctype html>
-<html>
+<html class="no-js" lang="">
 <head>
-<title>Endouble Insights</title>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title></title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="apple-touch-icon" href="apple-touch-icon.png">
+    <!-- Place favicon.ico in the root directory -->
+
+    <link rel="stylesheet" href="http://local.endouble-insights.com/css/main.css">
+
+
 </head>
 <body>
-Yoohoooo
+
+<header>
+    <h1>Insights</h1>
+</header>
+
+<main>
+    <section class="dynamic-map">
+        <?php include 'map.php'; ?>
+    </section>
+
+    <aside class="map-sidebar">
+        <?php // include 'box.php'; ?>
+    </aside>
+</main>
+
 </body>
 </html>
